d162d715e33136fa567a2e59912a7f29dbd7a1c6
Ryan Zec <code@ryanzec.com>
2015-04-20 16:20:57 -0400

    added missing file from previous commit

---
+++ assets/mixins/clickable.mixin.js
@@ -0,0 +1,58 @@
+var React = require('react');
+
+var clickableMixin = {};
+
+clickableMixin.propTypes = {
+  isClickable: React.PropTypes.bool,
+  onClick: React.PropTypes.func
+};
+
+clickableMixin.getDefaultProps = function clickableMixinGetDefaultProps() {
+  return {
+    isClickable: false,
+    onClick: null,
+    isQuiet: false
+  };
+};
+
+clickableMixin.getInitialState = function clickableMixinGetInitialState() {
+  return {
+    isPressed: false
+  };
+};
+
+clickableMixin.onMouseLeave = function clickableMixinOnMouseLeave() {
+  this.setState({
+    isPressed: false
+  });
+};
+
+clickableMixin.onMouseDown = function clickableMixinOnMouseDown() {
+  this.setState({
+    isPressed: true
+  });
+};
+
+clickableMixin.onMouseUp = function clickableMixinOnMouseUp() {
+  this.setState({
+    isPressed: false
+  });
+};
+
+clickableMixin.getEventHandlerProps = function clickableMixinGetEventHandlerProps() {
+  var props = {};
+
+  if (this.props.isClickable === true) {
+    if (this.props.onClick) {
+      props.onClick = this.props.onClick;
+    }
+
+    props.onMouseDown = this.onMouseDown;
+    props.onMouseUp = this.onMouseUp;
+    props.onMouseLeave = this.onMouseLeave;
+  }
+
+  return props;
+};
+
+module.exports = clickableMixin;
