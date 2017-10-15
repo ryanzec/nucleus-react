c215f99bd4d2a525330fd064d12fd01afa1b439c
Ryan Zec <code@ryanzec.com>
2017-10-15 19:57:52 -0400

    switched to ava for the unit test runner

---
+++ test/specs/src/utilities/array.spec.js
@@ -0,0 +1,23 @@
+import test from 'ava';
+
+import * as arrayUtility from '../../../../src/utilities/array';
+
+test('toggleArrayValue should add value when not present', (tester) => {
+  const expectedValue = [
+    'test',
+    'test2',
+    'test3',
+    'test4',
+  ];
+
+  tester.deepEqual(arrayUtility.toggleArrayValue(['test', 'test2', 'test3'], 'test4'), expectedValue);
+});
+
+test('toggleArrayValue should remove value when present', (tester) => {
+  const expectedValue = [
+    'test',
+    'test3',
+  ];
+
+  tester.deepEqual(arrayUtility.toggleArrayValue(['test', 'test2', 'test3'], 'test2'), expectedValue);
+});
