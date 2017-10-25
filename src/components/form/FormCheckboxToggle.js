450dd849d5c3154326cdd03bb7e647a9e31b9fa9
Ryan Zec <code@ryanzec.com>
2017-10-25 19:02:02 -0400

    converted teh checkbox toggle component to css modules

--- src/components/form/FormCheckboxToggle.js
+++ src/components/form/FormCheckboxToggle.js
@@ -10,19 +10,45 @@ import styles from 'src/components/form/FormCheckboxToggle.module.scss';
 export const createGetCssClasses = (instance) => {
   return () => {
     const composedStyles = composeStyles(styles, instance.props.customStyles);
-    let cssClasses = ['form-element__checkbox-toggle'];
+    let cssClasses = [composedStyles.container];
 
     if (instance.props.className) {
       cssClasses = cssClasses.concat(instance.props.className.split(' '));
     }
-
     if (instance.props.checked) {
-      cssClasses.push('is-checked');
+      cssClasses.push(composedStyles.isChecked);
     }
 
     return cssClasses.join(' ');
   };
-}
+};
+
+export const createGetToggleBarCssClasses = (instance) => {
+  return () => {
+    const composedStyles = composeStyles(styles, instance.props.customStyles);
+    let cssClasses = [composedStyles.toggleBar];
+
+    if (instance.props.checked) {
+      cssClasses.push(composedStyles.toggleBarIsChecked);
+    }
+
+    return cssClasses.join(' ');
+  };
+};
+
+export const createGetToggleCircleCssClasses = (instance) => {
+  return () => {
+    const composedStyles = composeStyles(styles, instance.props.customStyles);
+    let cssClasses = [composedStyles.toggleCircle];
+
+    if (instance.props.checked) {
+      cssClasses.push(composedStyles.toggleCircleIsChecked);
+    }
+
+    return cssClasses.join(' ');
+  };
+};
+
 
 class FormCheckboxToggle extends React.Component {
   static propTypes = {
@@ -30,7 +56,7 @@ class FormCheckboxToggle extends React.Component {
     checked: PropTypes.bool,
     offNode: PropTypes.node,
     onNode: PropTypes.node,
-    customStyles: PropTypes.object,
+    customStyles: PropTypes.object
   };
 
   static defaultProps = {
@@ -42,6 +68,8 @@ class FormCheckboxToggle extends React.Component {
   };
 
   getCssClasses = createGetCssClasses(this);
+  getToggleBarCssClasses = createGetToggleBarCssClasses(this);
+  getToggleCircleCssClasses = createGetToggleCircleCssClasses(this);
 
   render() {
     return (
@@ -49,8 +77,8 @@ class FormCheckboxToggle extends React.Component {
         className={this.getCssClasses()}
         {...getPassThroughProperties(this.props, FormCheckboxToggle.propTypes)}
       >
-        <div className="form-element__checkbox-toggle-circle"></div>
-        <div className="form-element__checkbox-toggle-bar">
+        <div className={this.getToggleCircleCssClasses()} />
+        <div className={this.getToggleBarCssClasses()}>
           <span>{this.props.checked ? this.props.onNode : this.props.offNode}</span>
         </div>
       </div>
