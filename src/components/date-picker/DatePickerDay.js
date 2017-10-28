6a84ef7c1787c5c846d8c7f9910e44da1340a26e
Ryan Zec <code@ryanzec.com>
2017-10-28 16:24:44 -0400

    date picker converted to css modules

--- src/components/date-picker/DatePickerDay.js
+++ src/components/date-picker/DatePickerDay.js
@@ -1,25 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {getPassThroughProperties} from 'src/utilities/component';
+import {
+  getPassThroughProperties,
+  composeStyles,
+} from 'src/utilities/component';
+
+import styles from 'src/components/date-picker/DatePickerDay.module.scss';
 
 export const createGetCssClasses = (instance) => {
   return () => {
-    let cssClasses = ['date-picker__day'];
+    const composedStyles = composeStyles(styles, instance.props.customStyles);
+    let cssClasses = [composedStyles.container];
 
     if (instance.props.className) {
       cssClasses = cssClasses.concat(instance.props.className.split(' '));
     }
 
     if (instance.props.isOtherMonth) {
-      cssClasses.push('m-other-month');
+      cssClasses.push(composedStyles.notCurrentMonth);
     }
 
     if (instance.props.isActive) {
-      cssClasses.push('is-active');
+      cssClasses.push(composedStyles.isActive);
     }
 
     if (instance.props.disabled) {
-      cssClasses.push('is-disabled');
+      cssClasses.push(composedStyles.isDisabled);
     }
 
     return cssClasses.join(' ');
@@ -43,6 +49,7 @@ export const createOnClick = (instance) => {
 class DatePickerDay extends React.Component {
   static propTypes = {
     className: PropTypes.string,
+    customStyles: PropTypes.object,
     isOtherMonth: PropTypes.bool,
     isActive: PropTypes.bool,
     day: PropTypes.object.isRequired,
@@ -53,6 +60,7 @@ class DatePickerDay extends React.Component {
 
   static defaultProps = {
     className: null,
+    customStyles: null,
     isOtherMonth: false,
     isActive: false,
     disabled: false,
